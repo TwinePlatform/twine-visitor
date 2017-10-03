50f29d85d441b88d566f3907c35fec56df8c8f08
RogeredBacon <jinglis12@googlemail.com>
2017-10-03 18:07:41 +0100

    Just started the queries #25

---
+++ react-backend/database/queries/getHash.js
@@ -0,0 +1,14 @@
+const dbConnection = require('../dbConnection');
+
+const checkName = 'SELECT fullname from users WHERE hash (VALUES ($1))';
+
+
+const getHash = (hashString, cb) => {
+  dbConnection.query(checkName, hashString, (err, res) => {
+    if (err) { return cb(err); }
+
+    cb(null, res);
+  });
+};
+
+module.exports = getHash;
