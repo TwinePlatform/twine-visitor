5ef33baad1072efc6bc339fbd32f1e95d574a998
alinasolinas <azayneeva@gmail.com>
2017-10-03 11:42:43 +0100

    added instascan

---
+++ react-backend/client/src/components/main_copy.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {Input} from './input';
+import {Select} from './select';
+import {Button} from './button';
+
+export class QRCode extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      login: 1,
+      username: '',
+      qrcode: ''
+    }
+  }
+
+  // handleChange = (e) => {
+  //   let newState = {};
+  //   newState[e.target.name] = e.target.value;
+  //   this.setState(newState);
+  // };
+  //
+  // handleSubmit = (e) => {
+  //   e.preventDefault();
+  //
+  //   let newState = {};
+  //   newState['formpage'] = this.state.formpage+1;
+  //   this.setState(newState);
+  //
+  //   let formData = {
+  //    formSender: this.state.username,
+  //    formEmail: this.state.email,
+  //    formSex: this.state.sex,
+  //    formyear: this.state.year
+  //     }
+  //
+  //   console.log(formData);
+  // }
+
+  render() {
+
+    if (this.state.login === 1) {
+      return (
+        <section className="Main">
+          <script type="text/javascript" src="./javascripts/instascan.min.js"></script>
+          <h1>WELCOME BACK!</h1>
+          <div id="instascan">
+            <video id="preview"></video>
+            <script src="./javascripts/instascan"></script>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="Main">
+          <h1>Please tell us about yourself</h1>
+          <form className="Signup" onChange={this.handleChange} onSubmit={this.handleSubmit}>
+            <Select question="Select Your Sex" option="sex" choices={['male', 'female', 'prefer not to say']}/>
+            <Select question="Year of Birth" option="year" choices={[
+              1980,
+              1981,
+              1982,
+              1983,
+              1984,
+              1985
+            ]}/>
+            <Button/>
+          </form>
+        </section>
+      )
+    }
+  }
+
+}
---
+++ react-backend/public/javascripts/instascan.js
@@ -0,0 +1,14 @@
+
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+      scanner.addListener('scan', function (content) {
+        console.log(content);
+      });
+      Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+          scanner.start(cameras[0]);
+        } else {
+          console.error('No cameras found.');
+        }
+      }).catch(function (e) {
+        console.error(e);
+      });
