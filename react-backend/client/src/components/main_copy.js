1fe793abdda2c7f7c270a0993a59ba2b3d7dcabe
RogeredBacon <jinglis12@googlemail.com>
2017-10-03 17:28:26 +0100

    Tidying up the files with airbnb eslint

--- react-backend/client/src/components/main_copy.js
+++ react-backend/client/src/components/main_copy.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
-import {Input} from './input';
-import {Select} from './select';
-import {Button} from './button';
+import React, { Component } from 'react';
+import { Input } from './input';
+import { Select } from './select';
+import { Button } from './button';
 
 export class QRCode extends Component {
-
   constructor() {
     super();
 
     this.state = {
       login: 1,
       username: '',
-      qrcode: ''
-    }
+      qrcode: '',
+    };
   }
 
   // handleChange = (e) => {
@@ -37,40 +36,41 @@ export class QRCode extends Component {
   //
   //   console.log(formData);
   // }
-  componentDidMount(){
+  componentDidMount() {
     window.instascan();
   }
 
   render() {
-
     if (this.state.login === 1) {
       return (
         <section className="Main">
           <h1>WELCOME BACK!</h1>
           <div id="instascan">
-            <video id="preview" className="Video active"></video>
+            <video id="preview" className="Video active" />
           </div>
         </section>
-      )
-    } else {
-      return (
-        <section className="Main">
-          <h1>Please tell us about yourself</h1>
-          <form className="Signup" onChange={this.handleChange} onSubmit={this.handleSubmit}>
-            <Select question="Select Your Sex" option="sex" choices={['male', 'female', 'prefer not to say']}/>
-            <Select question="Year of Birth" option="year" choices={[
+      );
+    }
+    return (
+      <section className="Main">
+        <h1>Please tell us about yourself</h1>
+        <form className="Signup" onChange={this.handleChange} onSubmit={this.handleSubmit}>
+          <Select question="Select Your Sex" option="sex" choices={['male', 'female', 'prefer not to say']} />
+          <Select
+            question="Year of Birth"
+            option="year"
+            choices={[
               1980,
               1981,
               1982,
               1983,
               1984,
-              1985
-            ]}/>
-            <Button/>
-          </form>
-        </section>
-      )
-    }
+              1985,
+            ]}
+          />
+          <Button />
+        </form>
+      </section>
+    );
   }
-
 }
--- react-backend/client/src/components/select.js
+++ react-backend/client/src/components/select.js
@@ -1,29 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export class Select extends Component {
   constructor(props) {
     super(props);
-    this.state = {userInput: ''};
+    this.state = { userInput: '' };
     this.handleUserInput = this.handleUserInput.bind(this);
   }
 
   handleUserInput(e) {
-    this.setState({userInput: e.target.value});
+    this.setState({ userInput: e.target.value });
   }
 
-  render(){
-
+  render() {
     return (
       <label>{this.props.question}
-      <br />
-      <select name = {this.props.option} onChange={this.handleUserInput} value={this.state.userInput}>
-      {this.props.choices.map(function(choice){
-          return <option key = {choice} value={choice}>{choice}</option>;
-        })}
+        <br />
+        <select name={this.props.option} onChange={this.handleUserInput} value={this.state.userInput}>
+          {this.props.choices.map(choice => <option key={choice} value={choice}>{choice}</option>)}
 
-      </select>
-      <br />
+        </select>
+        <br />
       </label>
-    )
+    );
   }
 }
