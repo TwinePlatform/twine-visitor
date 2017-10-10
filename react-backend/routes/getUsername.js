59034d9e4bcfb36a56d4b3cf11319c0321e15a9f
alinasolinas <azayneeva@gmail.com>
2017-10-10 23:40:32 +0100

    username error fixed

--- react-backend/routes/getUsername.js
+++ react-backend/routes/getUsername.js
@@ -5,16 +5,17 @@ const router = express.Router();
 const getHash = require('../database/queries/getHash');
 
 router.post('/', (req, res, next) => {
-  const hashToCheck = req.body.user
+  let hashToCheck = req.body.user
 
   if (!(validator.isHash(hashToCheck, ['sha256']))) {
-    return Promise.reject('WAS NOT HASH!')
+    hashToCheck = 'WAS NOT HASH!'
+    console.log('WAS NOT HASH!')
   }
 
   getHash(hashToCheck)
     .then(fullname => res.send({fullname}))
     .catch((err) => {
-      console.log("Look, I am a caught error ", err);
+      res.send({fullname: 'there is no registered user', hash: '0'})
     })
 });
 
