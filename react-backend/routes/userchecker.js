c7b8714ec8ccf40b8b5df60854d5ab7a4ed54cc3
RogeredBacon <jinglis12@googlemail.com>
2017-10-10 14:58:33 +0100

    Added validator module and implemented validation for email and full name

--- react-backend/routes/userchecker.js
+++ react-backend/routes/userchecker.js
@@ -1,4 +1,7 @@
+const validator = require('validator');
+
 const express = require('express');
+
 
 const router = express.Router();
 const getUserAlreadyExists = require('../database/queries/getUserAlreadyExists');
@@ -11,13 +14,20 @@ router.post('/', (req, res, next) => {
 
   req.on('end', () => {
     const data = JSON.parse(body);
-    getUserAlreadyExists(data.formSender, data.formEmail, (error, result) => {
-      if (error) {
-        console.log('error from getUserAlreadyExists ', error)
-      } else {
-        res.send(result.rows[0].exists);
-      }
-    });
+    if ((validator.isEmail(data.formEmail)) && (validator.isAlpha(data.formSender, ['en-GB']))) {
+      // console.log(data.formSender);
+      // console.log(validator.isAlpha(`${data.formSender}`, ['en-GB']));
+      getUserAlreadyExists(data.formSender, data.formEmail, (error, result) => {
+        if (error) {
+          console.log('error from getUserAlreadyExists ', error);
+        } else {
+          res.send(result.rows[0].exists);
+        }
+      });
+    } else {
+      res.send(true);
+      // console.log(validator.isAlpha(`${data.formSender}`, ['en-GB']));
+    }
   });
 });
 
