1ab877782ede5c2d4344bfba415b3a352ae55b42
RogeredBacon <jinglis12@googlemail.com>
2017-10-10 16:29:08 +0100

    Added validation for the qrcode reader and tidyed things up a bit with linter #3 #2 #17

--- react-backend/client/src/components/qrerror.js
+++ react-backend/client/src/components/qrerror.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
-import {Route, Switch, Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
 export class QrError extends Component {
   render() {
     return (
       <section className="Main">
-          <h1>We’re sorry, there was a problem scanning your code</h1>
-          <h2>Please choose one of these options or go  to Reception for help</h2>
-          <Link to="/signup">
-            <button className="Button">Register as a new user</button>
-          </Link><br/>
-          <Link to="/flow2">
-            <button className="Button">Try to scan QR code again</button>
-          </Link><br/>
-        </section>
-      )
+        <h1>We’re sorry, there was a problem scanning your code</h1>
+        <h2>Please choose one of these options or go  to Reception for help</h2>
+        <Link to="/signup">
+          <button className="Button">Register as a new user</button>
+        </Link><br />
+        <Link to="/flow2">
+          <button className="Button">Try to scan QR code again</button>
+        </Link><br />
+      </section>
+    );
   }
 }
--- react-backend/functions/qrcodemaker.js
+++ react-backend/functions/qrcodemaker.js
@@ -1,13 +1,13 @@
 const QrCode = require('qrcode');
 
-module.exports = (string) => new Promise(function(resolve, reject) {
-  console.log("Im running");
+module.exports = string => new Promise(((resolve, reject) => {
+  console.log('Im running');
   QrCode.toDataURL(string, {
-    errorCorrectionLevel: 'H'
-  }, function(err, url) {
+    errorCorrectionLevel: 'H',
+  }, (err, url) => {
     if (err) {
-      return reject(err)
+      return reject(err);
     }
     resolve(url);
-  })
-});
+  });
+}));
