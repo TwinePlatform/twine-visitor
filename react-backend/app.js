699218beceaeb1cafb975159e665972138e84738
rachaelcodes <rorourke.rsp@gmail.com>
2017-10-11 12:50:02 +0100

    #50 push right branch

--- react-backend/app.js
+++ react-backend/app.js
@@ -25,7 +25,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use('/', index);
 app.use('/users', users);
