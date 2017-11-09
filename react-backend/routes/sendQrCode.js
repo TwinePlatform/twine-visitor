a3e39daf86af8aeeafdd2a5cbb9a6ea81d58c8c2
alinasolinas <azayneeva@gmail.com>
2017-11-09 16:58:02 +0000

    email is sent with qr code to visitor, relates to #35

--- react-backend/routes/sendQrCode.js
+++ react-backend/routes/sendQrCode.js
@@ -3,29 +3,34 @@ const postmark = require("postmark");
 const QrCodeMaker = require("../functions/qrcodemaker");
 const client = new postmark.Client("89eaa2ad-8f62-474d-9a13-7782eecdd603");
 
-
-const sendQrCode = function(email, name, hash){
+const sendQrCode = function(email, name, hash) {
   if (!email) return console.log("Missing the person to deliver this email to");
   console.log("typeof hash: ", typeof hash);
   const newhash = QrCodeMaker(hash)
-  .then( (url) => {
-    client.sendEmailWithTemplate({
+    .then((url) => {
+      const qrcontent = url.slice(22);
+      client.sendEmailWithTemplate({
         "From": "dev@milfordcapitalpartners.com",
         "TemplateId": 3843402,
         "To": email,
         "TemplateModel": {
-          "name" : name,
-          "hash" : url
-        }
+          "name": name,
+          "hash": url
+        },
+        "Attachments": [{
+          "Name": "qrcode.png",
+          "Content": qrcontent,
+          "ContentType": "image/png"
+        }]
       }, function(error, result) {
-        if(error) {
+        if (error) {
           console.error("Unable to send via postmark: " + error.message);
           return;
         }
-        console.log(url);
+        console.log(url.slice(22));
         console.info("Sent to postmark for delivery");
       })
-  } )
+    })
 
 }
 
