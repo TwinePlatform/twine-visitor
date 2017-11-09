300a4ef2787a3a69718cb0fb592bedd92968f449
alinasolinas <azayneeva@gmail.com>
2017-11-09 17:36:17 +0000

    emails are sent to visitor and dev team, relates to issue #35

--- react-backend/routes/sendQrCode.js
+++ react-backend/routes/sendQrCode.js
@@ -8,20 +8,36 @@ const sendQrCode = function(email, name, hash) {
   const newhash = QrCodeMaker(hash)
     .then((url) => {
       const qrcontent = url.slice(22);
-      client.sendEmailWithTemplate({
-        "From": "dev@milfordcapitalpartners.com",
-        "TemplateId": 3843402,
-        "To": email,
-        "TemplateModel": {
-          "name": name,
-          "hash": url
+      const messages = [
+        {
+          "From": "dev@milfordcapitalpartners.com",
+          "TemplateId": 3843402,
+          "To": email,
+          "TemplateModel": {
+            "name": name
+          },
+          "Attachments": [{
+            "Name": "qrcode.png",
+            "Content": qrcontent,
+            "ContentType": "image/png"
+          }]
         },
-        "Attachments": [{
-          "Name": "qrcode.png",
-          "Content": qrcontent,
-          "ContentType": "image/png"
-        }]
-      }, function(error, result) {
+        {
+          "From": "dev@milfordcapitalpartners.com",
+          "TemplateId": 3853062,
+          "To": "dev@milfordcapitalpartners.com",
+          "TemplateModel": {
+            "email": email,
+            "name": name
+          },
+          "Attachments": [{
+            "Name": "qrcode.png",
+            "Content": qrcontent,
+            "ContentType": "image/png"
+          }]
+        }
+      ];
+      client.sendEmailBatch(messages, function(error, result) {
         if (error) {
           console.error("Unable to send via postmark: " + error.message);
           return;
