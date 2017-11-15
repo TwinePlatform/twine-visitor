406678234c794d935ee3cacf0b93655d0f253136
RogeredBacon <jinglis12@googlemail.com>
2017-11-15 15:56:40 +0000

    Reformatted the pdf + changed QRCode colour #16

--- react-backend/functions/qrcodemaker.js
+++ react-backend/functions/qrcodemaker.js
@@ -3,6 +3,11 @@ const QrCode = require('qrcode');
 module.exports = string => new Promise((resolve, reject) => {
   QrCode.toDataURL(string, {
     errorCorrectionLevel: 'H',
+    color: {
+
+      dark: '#06112f',
+      light: '#FFFFFF',
+    },
   }, (err, url) => {
     if (err) {
       return reject(err);
--- react-backend/routes/sendQrCode.js
+++ react-backend/routes/sendQrCode.js
@@ -3,16 +3,16 @@ const generatePdf = require('../functions/pdfgenerator');
 const sendemail = require('../functions/sendemail');
 
 
-const sendQrCode = function(email, name, hash) {
+const sendQrCode = function (email, name, hash) {
   if (!email) return console.log('Missing the person to deliver this email to');
   QrCodeMaker(hash)
     .then(generatePdf)
-    .then(pdf=>sendemail(email, name, pdf))
-    .catch(err => {
-      console.log(err)
-    })
-}
+    .then(pdf => sendemail(email, name, pdf))
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
-  sendQrCode
+  sendQrCode,
 };
