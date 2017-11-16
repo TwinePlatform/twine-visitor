d8c82741c1d68e7303c4b071372ec3ba1f27990d
RogeredBacon <jinglis12@googlemail.com>
2017-11-16 13:04:48 +0000

    Added the config.env variable for cb_email

--- react-backend/functions/sendemail.js
+++ react-backend/functions/sendemail.js
@@ -6,39 +6,39 @@ if (!process.env.POSTMARK_SERVER) throw new Error('Environment variable POSTMARK
 const client = new postmark.Client(process.env.POSTMARK_SERVER);
 
 module.exports = (email, name, pdf) => {
-    const messages = [{
-        From: 'sonjaw@powertochange.org.uk',
-        TemplateId: 3843402,
-        To: email,
-        TemplateModel: {
-          name,
-        },
-        Attachments: [{
-          Name: 'qrcode.pdf',
-          Content: pdf,
-          ContentType: 'application/octet-stream'
-        }],
-      },
-      {
-        From: 'sonjaw@powertochange.org.uk',
-        TemplateId: 3853062,
-        To: 'dev@milfordcapitalpartners.com',
-        TemplateModel: {
-          email,
-          name,
-        },
-        Attachments: [{
-          Name: 'qrcode.pdf',
-          Content: pdf,
-          ContentType: 'application/octet-stream'
-        }],
-      }
-    ];
-    client.sendEmailBatch(messages, (error, result) => {
-      if (error) {
-        console.error('Unable to send via postmark: ', error);
-        return;
-      }
-      console.info('Sent to postmark for delivery', result);
-    });
-}
+  const messages = [{
+    From: 'visitorapp@powertochange.org.uk',
+    TemplateId: 3843402,
+    To: email,
+    TemplateModel: {
+      name,
+    },
+    Attachments: [{
+      Name: 'qrcode.pdf',
+      Content: pdf,
+      ContentType: 'application/octet-stream',
+    }],
+  },
+  {
+    From: 'visitorapp@powertochange.org.uk',
+    TemplateId: 3853062,
+    To: process.env.CB_EMAIL,
+    TemplateModel: {
+      email,
+      name,
+    },
+    Attachments: [{
+      Name: 'qrcode.pdf',
+      Content: pdf,
+      ContentType: 'application/octet-stream',
+    }],
+  },
+  ];
+  client.sendEmailBatch(messages, (error, result) => {
+    if (error) {
+      console.error('Unable to send via postmark: ', error);
+      return;
+    }
+    console.info('Sent to postmark for delivery', result);
+  });
+};
