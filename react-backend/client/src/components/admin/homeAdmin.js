e589d6fbcb941055bc3a6d7c123c77cae57fe85e
Zooey <zooeyxmiller@gmail.com>
2017-12-06 15:46:28 +0000

    adds reauthentication to admin dashboard #78

--- react-backend/client/src/components/admin/homeAdmin.js
+++ react-backend/client/src/components/admin/homeAdmin.js
@@ -1,40 +1,64 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class HomeAdmin extends Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
+      reauthenticated: false,
+      failure: false,
+      password: ""
     };
   }
 
-  componentDidMount() {
+  handleChange = e => {
+    let newState = {};
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  };
+
+  authenticate = event => {
+    event.preventDefault();
     const headers = new Headers({
-      Authorization: localStorage.getItem('token'),
+      Authorization: localStorage.getItem("token")
     });
-    fetch('/all-users', {
-      method: 'GET',
+    fetch("/all-users", {
+      method: "POST",
       headers,
+      body: JSON.stringify({ password: this.state.password })
     })
       .then(res => res.json())
-      .then((res) => {
-        if (res.error) {
-          throw res.error;
-        } else {
+      .then(res => {
+        console.log(res);
+        if (res.success) {
           return res.users;
+        } else {
+          if (res.reason === "not logged in") {
+            throw new Error("not logged in");
+          } else {
+            this.setState({ failure: true });
+            throw new Error("password");
+          }
         }
       })
-      .then((users) => {
-        this.setState({ users });
+      .then(users => {
+        console.log(users);
+        this.setState({ users, reauthenticated: true });
       })
-      .catch((err) => {
-        this.props.history.push('/logincb');
+      .catch(err => {
+        if (!this.state.failure) {
+          this.props.history.push("/logincb");
+        }
       });
-  }
+  };
+
+  passwordError = (
+    <span>The email address or password is incorrect. Please try again.</span>
+  );
 
   render() {
-    return (
+    return this.state.reauthenticated ? (
       <div>
         <h2>Visitor Data</h2>
         <table>
@@ -65,6 +89,22 @@ export class HomeAdmin extends Component {
           <button className="ButtonBack">Back to the main page</button>
         </Link>
         <br />
+      </div>
+    ) : (
+      <div>
+        <form onSubmit={this.authenticate}>
+          <label>
+            password
+            <input
+              type="text"
+              name="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </label>
+          <button>login</button>
+        </form>
+        {this.state.failure ? this.passwordError : ""}
       </div>
     );
   }
