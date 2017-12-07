7aeed677a2d6e9b5848955373c5a1fe7c42d5b3b
RogeredBacon <jinglis12@googlemail.com>
2017-12-07 18:08:13 +0000

    Slightly changed the putNewPassword query to reset the token expiry time to 0 so that each token can only be used once #79

--- react-backend/database/queries/CBqueries/putNewPassword.js
+++ react-backend/database/queries/CBqueries/putNewPassword.js
@@ -1,6 +1,7 @@
 const dbConnection = require('../../dbConnection');
 
-const insertNewPassword = 'UPDATE cbusiness SET hash_pwd = $1 WHERE token = $2';
+const insertNewPassword =
+  'UPDATE cbusiness SET hash_pwd = $1, tokenexpire = 0 WHERE token = $2';
 
 const putNewPassword = (password, token, cb) => {
   dbConnection.query(insertNewPassword, [password, token], (err, res) => {
