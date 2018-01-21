57b699d0e03ebafbd305b7c3f6cc9b1c0e10ea1a
alinasolinas <azayneeva@gmail.com>
2018-01-21 20:21:58 +0000

    used module node-base64-image to send pdf attachment with the actual logo, because pdfmake module doesn not accept external url for images, had to encode it in base64

--- react-backend/functions/pdfgenerator.js
+++ react-backend/functions/pdfgenerator.js
@@ -1,67 +1,130 @@
 const PdfPrinter = require('pdfmake');
+const base64 = require('node-base64-image');
 
-module.exports = QRcodeBase64Url => {
-  const fontDescriptors = {
-    Roboto: {
-      normal: `${__dirname}/../public/Roboto/Roboto-Regular.ttf`,
-      bold: `${__dirname}/../public/Roboto/Roboto-Medium.ttf`,
-      italics: `${__dirname}/../public/Roboto/Roboto-Italic.ttf`,
-      bolditalics: `${__dirname}/../public/Roboto/Roboto-MediumItalic.ttf`,
-    },
-  };
+module.exports = (QRcodeBase64Url, image) =>
+  new Promise((resolve, reject) => {
+    const fontDescriptors = {
+      Roboto: {
+        normal: `${__dirname}/../public/Roboto/Roboto-Regular.ttf`,
+        bold: `${__dirname}/../public/Roboto/Roboto-Medium.ttf`,
+        italics: `${__dirname}/../public/Roboto/Roboto-Italic.ttf`,
+        bolditalics: `${__dirname}/../public/Roboto/Roboto-MediumItalic.ttf`,
+      },
+    };
+    if (image) {
+      // need to download the logo locally
+      base64.encode(image, { string: true }, (err, image) => {
+        if (err) {
+          return reject(err);
+        }
 
-  const docDefinition = {
-    info: {
-      title: 'Visitor QRCode',
-      author: 'Power To Change',
-    },
+        console.log(`data:image/png;base64, ${image}`);
+        // resolve(QRcodeBase64Url, image)
 
-    pageSize: 'C8',
-    pageOrientation: 'landscape',
-    pageMargins: [5, 5, 5, 5],
+        const docDefinition = {
+          info: {
+            title: 'Visitor QRCode',
+            author: 'Power To Change',
+          },
 
-    content: [
-      {
-        columns: [
+          pageSize: 'C8',
+          pageOrientation: 'landscape',
+          pageMargins: [5, 5, 5, 5],
+
+          content: [
+            {
+              columns: [
+                {
+                  image: `data:image/png;base64, ${image}`,
+                  margin: [5, 5, 0, 0],
+                  fit: [100, 100],
+                },
+                {
+                  image: QRcodeBase64Url,
+                  margin: [-5, -3, 5, 0],
+                  fit: [115, 115],
+                },
+              ],
+            },
+            {
+              text: 'Use this QR Code to sign in',
+              fontSize: 14,
+              alignment: 'center',
+              margin: [0, 10, 10, 0],
+              color: '#0a092a',
+            },
+          ],
+        };
+
+        const printer = new PdfPrinter(fontDescriptors);
+
+        const doc = printer.createPdfKitDocument(docDefinition);
+
+        const chunks = [];
+        let result;
+
+        doc.on('data', chunk => {
+          chunks.push(chunk);
+        });
+        doc.on('end', () => {
+          result = Buffer.concat(chunks);
+          resolve(result.toString('base64'));
+          reject("Couldn't generate PDF");
+        });
+        doc.end();
+      });
+    } else {
+      const docDefinition = {
+        info: {
+          title: 'Visitor QRCode',
+          author: 'Power To Change',
+        },
+
+        pageSize: 'C8',
+        pageOrientation: 'landscape',
+        pageMargins: [5, 5, 5, 5],
+
+        content: [
           {
-            image: `${__dirname}/../public/qrcodelogo.png`,
-            width: 38,
-            height: 129.5,
-            margin: [0, 10, 0, 0],
+            columns: [
+              {
+                image: `${__dirname}/../public/qrcodelogo.png`,
+                width: 38,
+                height: 129.5,
+                margin: [0, 10, 0, 0],
+              },
+              {
+                image: QRcodeBase64Url,
+                margin: [25, 5, 0, 0],
+                fit: [125, 125],
+              },
+            ],
           },
           {
-            image: QRcodeBase64Url,
-            margin: [25, 5, 0, 0],
-            fit: [125, 125],
+            text: 'Use this QR Code to sign in',
+            fontSize: 14,
+            alignment: 'right',
+            margin: [0, -10, 10, 0],
+            color: '#0a092a',
           },
         ],
-      },
-      {
-        text: 'Use this QR Code to sign in',
-        fontSize: 14,
-        alignment: 'right',
-        margin: [0, -10, 10, 0],
-        color: '#0a092a',
-      },
-    ],
-  };
+      };
 
-  return new Promise((resolve, reject) => {
-    const printer = new PdfPrinter(fontDescriptors);
+      const printer = new PdfPrinter(fontDescriptors);
 
-    const doc = printer.createPdfKitDocument(docDefinition);
+      const doc = printer.createPdfKitDocument(docDefinition);
 
-    const chunks = [];
-    let result;
+      const chunks = [];
+      let result;
 
-    doc.on('data', chunk => {
-      chunks.push(chunk);
-    });
-    doc.on('end', () => {
-      result = Buffer.concat(chunks);
-      resolve(result.toString('base64'));
-      reject("Couldn't generate PDF");
-    });
-    doc.end();
+      doc.on('data', chunk => {
+        chunks.push(chunk);
+      });
+      doc.on('end', () => {
+        result = Buffer.concat(chunks);
+        resolve(result.toString('base64'));
+        reject("Couldn't generate PDF");
+      });
+      doc.end();
+    }
   });
-};
