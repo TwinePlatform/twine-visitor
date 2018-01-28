b5f76d0deb712bdea323c287ad24e0a1e1559c64
Finn Hodgkin <finnhodgkin@gmail.com>
2018-01-28 13:59:25 +0000

    Makes the admin fetch helpers more functional and get token from headers
    #127

--- react-backend/client/src/components/admin/activitiesLib/admin_helpers.js
+++ react-backend/client/src/components/admin/activitiesLib/admin_helpers.js
@@ -1,117 +1,86 @@
 const handleFetchError = (res) => {
   if (res.status === 500) throw new Error(500);
   if (res.status === 401) throw new Error(401);
-  return res;
 };
 
-export const authenticatedPost = (url, body) =>
-  new Promise((resolve, reject) => {
+const handleAuthenticatedFetch = (res) => {
+  const token = res.headers.get('authorization');
+
+  if (!token) throw new Error('No admin token');
+  if (res.status === 500) throw new Error(500);
+  if (res.status === 401) throw new Error(401);
+
+  return { res, token };
+};
+
+const authenticatedFetch = method => async (url, body) => {
+  try {
     const token = localStorage.getItem('token');
+    if (!token) throw new Error('Not logged in');
 
-    if (!token) return reject(new Error('Not logged in'));
+    const headers = {
+      Authorization: token,
+      'Content-Type': 'application/json',
+    };
+    const opts = body
+      ? { method, headers, body: JSON.stringify(body) }
+      : { method, headers };
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-      .then(handleFetchError)
-      .then(res => res.json())
-      .then(resolve)
-      .catch(reject);
-  });
+    const response = await fetch(url, opts);
+    handleFetchError(response);
 
-export const adminPost = (that, url, body) =>
-  new Promise((resolve, reject) => {
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const authenticatedPost = async (url, body) => {
+  try {
+    const response = await authenticatedFetch('POST')(url, body);
+    const responseBody = response.json();
+    return responseBody;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const adminFetch = method => async (that, url, body) => {
+  try {
     const adminToken = that.props.auth;
+    if (!adminToken) throw new Error('No admin token');
 
-    if (!adminToken) return reject(new Error('Not logged in'));
+    const headers = {
+      Authorization: adminToken,
+      'Content-Type': 'application/json',
+    };
+    const opts = body
+      ? { headers, method, body: JSON.stringify(body) }
+      : { headers, method };
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: adminToken,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-      .then(handleFetchError)
-      .then(res => res.json())
-      .then((res) => {
-        if (res.token) {
-          return that.props.updateAdminToken(res.token).then(() => {
-            resolve(res);
-          });
-        }
+    const response = await fetch(url, opts);
+    const { res, token } = handleAuthenticatedFetch(response);
 
-        throw new Error('No admin token');
-      })
-      .catch((error) => {
-        that.props.updateAdminToken('');
-        reject(error);
-      });
-  });
+    const responseData = await res.json();
 
-export const adminGet = (that, url, body) =>
-  new Promise((resolve, reject) => {
-    const adminToken = that.props.auth;
+    await that.props.updateAdminToken(token);
 
-    if (!adminToken) return reject(new Error('Not logged in'));
+    return responseData;
+  } catch (err) {
+    const redirectTo =
+      {
+        500: '/internalServerError',
+        401: '/admin/login',
+        'No admin token': '/admin/login',
+      }[err.message] || '/internalServerError';
 
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: adminToken,
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(handleFetchError)
-      .then(res => res.json())
-      .then((res) => {
-        if (res.token) {
-          return that.props.updateAdminToken(res.token).then(() => {
-            resolve(res);
-          });
-        }
+    that.props.history.push(redirectTo);
+    throw err;
+  }
+};
 
-        throw new Error('No admin token');
-      })
-      .catch((error) => {
-        that.props.updateAdminToken('');
-        reject(error);
-      });
-  });
+export const adminPost = adminFetch('POST');
+export const adminGet = adminFetch('GET');
 
 export const checkAdmin = that =>
-  new Promise((resolve, reject) => {
-    const adminToken = that.props.auth;
-
-    if (!adminToken) return reject(new Error('No admin token'));
-
-    fetch('/check-admin-token', {
-      method: 'POST',
-      headers: {
-        Authorization: adminToken,
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(handleFetchError)
-      .then(res => res.json())
-      .then((res) => {
-        const { success, token, error } = res;
-
-        if (success && token) {
-          return that.props.updateAdminToken(token).then(resolve);
-        }
-
-        const errorMessage = error || 'Unknown error';
-        throw new Error(errorMessage);
-      })
-      .catch((error) => {
-        that.props.updateAdminToken('');
-        reject(error);
-      });
-  });
+  adminFetch('POST')(that, '/check-admin-token');
