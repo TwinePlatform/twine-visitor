7044640cf8b64a7a5dfcee39ac865ebe69addd64
Finn Hodgkin <finnhodgkin@gmail.com>
2018-02-06 12:01:43 +0000

    Swaps logout button to MUI #179

--- react-backend/client/src/components/visitors/logoutbutton.js
+++ react-backend/client/src/components/visitors/logoutbutton.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class Logoutbutton extends Component {
   logout = () => {
     localStorage.removeItem('token');
-    localStorage.removeItem('adminToken');
     this.props.updateLoggedIn();
-    this.props.redirectUser('/logincb');
   };
 
   render() {
-    return (
-      <button className="Logoutbutton" type="submit" onClick={this.logout}>
-        Log out
-      </button>
-    );
+    return <RaisedButton label="Log out" onClick={this.logout} />;
   }
 }
