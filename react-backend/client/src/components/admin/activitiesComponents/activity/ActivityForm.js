ad9f56f0711919bf3d17e3bcafc81fa8cd2ca002
Finn Hodgkin <finnhodgkin@gmail.com>
2018-02-06 12:07:37 +0000

    Switches the Activity form to MUI #179

--- react-backend/client/src/components/admin/activitiesComponents/activity/ActivityForm.js
+++ react-backend/client/src/components/admin/activitiesComponents/activity/ActivityForm.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import ActionDone from 'material-ui/svg-icons/action/done';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import { List } from 'material-ui/List';
+import { cyan500 } from 'material-ui/styles/colors';
 
-export const ActivityForm = props => (
-  <form onSubmit={props.handleSubmit}>
-    <label className="Form__Label">
-      Add a new activity
-      <br />
-      <input
-        className="Form__Input"
-        type="text"
-        onChange={props.handleInputChange}
-        value={props.currentActivity}
-      />
-    </label>
-  </form>
+export const ActivityForm = ({
+  handleSubmit,
+  errorMessage,
+  handleInputChange,
+  currentActivity,
+}) => (
+  <List>
+    <form onSubmit={handleSubmit}>
+      <div style={{ paddingLeft: 16, display: 'flex', alignItems: 'center' }}>
+        <TextField
+          hintText="Activity name..."
+          floatingLabelText="Add a new activity"
+          floatingLabelStyle={{ color: cyan500 }}
+          errorStyle={{ float: 'left' }}
+          errorText={errorMessage}
+          onChange={handleInputChange}
+          value={currentActivity}
+          autoFocus
+        />
+        <FlatButton
+          style={{ marginTop: 25, marginLeft: 20 }}
+          label="Add"
+          type="submit"
+          icon={<ActionDone />}
+          primary
+        />
+      </div>
+    </form>
+  </List>
 );
