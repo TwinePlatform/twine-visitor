e6de97e7003999cc5dc7210735082c52f505f8f5
Finn Hodgkin <finnhodgkin@gmail.com>
2018-02-06 12:12:50 +0000

    Refactors admin menu page to MUI #179

--- react-backend/client/src/components/admin/adminMenuPage.js
+++ react-backend/client/src/components/admin/adminMenuPage.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Logoutbutton } from '../visitors/logoutbutton';
+import { List, ListItem } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+
 import { checkAdmin } from './activitiesLib/admin_helpers';
 
 export class AdminMenuPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      auth: 'PENDING',
       errorMessage: '',
     };
   }
 
   componentDidMount() {
     checkAdmin(this)
-      .then(() => this.setState({ auth: 'SUCCESS' }))
-      .catch(error => {
-        if (error.message === 500) {
-          this.props.history.push('/internalServerError');
-        } else if (error.message === 'No admin token') {
-          this.props.history.push('/admin/login');
-        } else {
-          this.props.history.push('/admin/login');
-        }
-      });
+      .then(() => null)
+      .catch(error => null);
   }
 
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -33,39 +26,36 @@ export class AdminMenuPage extends Component {
   };
 
   render() {
-    return this.state.auth === 'SUCCESS' ? (
-      <div>
-        <h1>
-          Welcome admin! <br /> Where do you want to go?<br />
-        </h1>
-        <Link to="/admin/activities">
-          <button className="Button">Activities</button>
-        </Link>
-        <br />
-        <Link to="/admin/visits">
-          <button className="Button">Visits</button>
-        </Link>
-        <br />
-        <Link to="/admin/users">
-          <button className="Button">Users</button>
-        </Link>
-        <br />
-        <Link to="/admin/accountSettings">
-          <button className="Button">Account Settings</button>
-        </Link>
-        <br />
-        <Link to="/" onClick={this.removeAdmin}>
-          <button className="Button ButtonBack">Back to the main page</button>
-        </Link>
-        <br />
-        <Logoutbutton
-          updateLoggedIn={this.props.updateLoggedIn}
-          redirectUser={this.props.history.push}
-        />
-        <br />
-      </div>
-    ) : (
-      <div> CHECKING ADMIN... </div>
+    return (
+      <React.Fragment>
+        <List style={{ textAlign: 'left' }}>
+          <Subheader>Admin Settings</Subheader>
+          <Link to="/admin/activities" style={{ textDecoration: 'none' }}>
+            <ListItem
+              primaryText="Activities"
+              secondaryText="Edit your community business activities"
+            />
+          </Link>
+          <Link to="/admin/visits" style={{ textDecoration: 'none' }}>
+            <ListItem
+              primaryText="Visits"
+              secondaryText="View information about visitors"
+            />
+          </Link>
+          <Link to="/admin/users" style={{ textDecoration: 'none' }}>
+            <ListItem
+              primaryText="Users"
+              secondaryText="Edit user information and re-email or print user QR codes"
+            />
+          </Link>
+          <Link to="/admin/accountSettings" style={{ textDecoration: 'none' }}>
+            <ListItem
+              primaryText="Account Settings"
+              secondaryText="Change your community business information and add a logo"
+            />
+          </Link>
+        </List>
+      </React.Fragment>
     );
   }
 }
