6f5f38e9c6f4130b899a0c5a9aaf5ffba5f5d298
RogeredBacon <jinglis12@googlemail.com>
2018-02-26 21:11:10 +0000

    Fixed tests

--- tests/routes_tests/visitor_name.test.js
+++ tests/routes_tests/visitor_name.test.js
@@ -7,7 +7,7 @@ test('POST /api/user/name-from-scan | viable & registered hash', (t) => {
   const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
 
   const successPayload = {
-    hashToCheck:
+    user:
       '9fb59d630d2fb12f7478c56c5f1b2fff20e0dd7c9d3a260eee7308a8eb6cd955',
   };
   request(app)
@@ -16,9 +16,8 @@ test('POST /api/user/name-from-scan | viable & registered hash', (t) => {
     .send(successPayload)
     .expect(200)
     .expect('Content-Type', /json/)
-    .expect(res => res.json().fullname)
     .end((err, res) => {
-      t.ok(res === 'james bond', 'Hash is viable and found in database');
+      t.equal(res.body.fullname, 'james bond');
       t.end();
     });
 });
@@ -27,7 +26,7 @@ test('POST /api/user/name-from-scan | non-viable hash ', (t) => {
   const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
 
   const failPayload = {
-    hashToCheck: 'Im fairly sure, though not positive, that this is not a hash',
+    user: 'Im fairly sure, though not positive, that this is not a hash',
   };
   request(app)
     .post('/api/user/name-from-scan')
@@ -35,9 +34,8 @@ test('POST /api/user/name-from-scan | non-viable hash ', (t) => {
     .send(failPayload)
     .expect(400)
     .expect('Content-Type', /json/)
-    .expect(res => res.json().fullname)
     .end((err, res) => {
-      t.notOk(res === 'Bad hash', 'Validation returned output of "Bad Hash"');
+      t.equal(res.body.fullname, 'Bad hash');
       t.end();
     });
 });
@@ -46,7 +44,7 @@ test('POST /api/user/name-from-scan | user/hash not registered ', (t) => {
   const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, process.env.SECRET);
 
   const failPayload = {
-    hashToCheck:
+    user:
       '5e6188b3f09e0d58acfbe4171284dd10b69ea8a78189f7bb8c7d6fa983557492',
   };
   request(app)
@@ -55,12 +53,9 @@ test('POST /api/user/name-from-scan | user/hash not registered ', (t) => {
     .send(failPayload)
     .expect(401)
     .expect('Content-Type', /json/)
-    .expect(res => res.json().fullname)
     .end((err, res) => {
-      t.notOk(
-        res === 'there is no registered user',
-        'There is no user registered with that hash"'
-      );
+      t.equal(
+        res.body.fullname, 'there is no registered user');
       t.end();
     });
 });
