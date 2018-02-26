f6063041a3fbb831392ee4207d56367fec377543
Elias Malik <elias0789@gmail.com>
2018-02-26 17:40:17 +0000

    🎨 Add config processing functionality
    The config directory will contain all configuration-related
    functionality.
    
    Currently the defaults and schema for the config object
    are stored here. The functions defined in config/index.js
    will be later used in the application entry point.
    
    Related to #190

---
+++ config/config.schema.js
@@ -0,0 +1,38 @@
+/*
+ * Configuration object schema
+ *
+ * Specifies the required shape and content of the configuration object.
+ */
+const Joi = require('joi');
+
+module.exports = {
+  env: Joi.string().only('dev', 'test', 'prod'),
+  web: Joi.object({
+    host: Joi.string().min(1),
+    port: Joi.number().min(0).max(65535).required(),
+    tls: Joi.alternatives().try(
+      Joi.only(null),
+      Joi.object({
+        key: Joi.string().required(),
+        cert: Joi.string().required(),
+      }),
+    ),
+  }),
+  psql: Joi.object({
+    host: Joi.string().min(1).required(),
+    port: Joi.number().min(0).max(65535).required(),
+    database: Joi.string().min(1).required(),
+    user: Joi.string().min(1).required(),
+    password: Joi.string(),
+    ssl: Joi.bool().default(false),
+  }),
+  email: Joi.object({
+    postmark_key: Joi.string().required(),
+    twine_email: Joi.string().email().required(),
+  }),
+  session: Joi.object({
+    jwt_secret: Joi.string().min(20).required(),
+    hmac_secret: Joi.string().min(20).required(),
+    ttl: Joi.number().integer().min(0).max(3e10), // milliseconds
+  }),
+};
