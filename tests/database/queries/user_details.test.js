177b57fcfa8f5d7a581b2691e03d8ee60b0888f5
RogeredBacon <jinglis12@googlemail.com>
2018-03-13 15:15:16 +0000

    Fixed test failures due to schema changes #218

--- tests/database/queries/user_details.test.js
+++ tests/database/queries/user_details.test.js
@@ -23,7 +23,7 @@ test('DB Query | user_details', async tape => {
           sex: 'male',
           yearofbirth: 1984,
           email: 'hello@yahoo.com',
-          phone: '7538654284',
+          phone_number: '7538654284',
           date: new Date('Mon May 15 2017 12:24:57 GMT+0000 (UTC)'),
           hash:
             '9fb59d630d2fb12f7478c56c5f1b2fff20e0dd7c9d3a260eee7308a8eb6cd955',
--- tests/database/queries/user_details_update.test.js
+++ tests/database/queries/user_details_update.test.js
@@ -33,7 +33,7 @@ test('DB Query | user_details_update', async tape => {
         sex: 'female',
         yearofbirth: 2001,
         email: 'not@makingsense.com',
-        phone: '07534532459',
+        phone_number: '07534532459',
         date: new Date('Mon May 15 2017 12:24:57 GMT+0000 (UTC)'),
         hash:
           '9fb59d630d2fb12f7478c56c5f1b2fff20e0dd7c9d3a260eee7308a8eb6cd955',
