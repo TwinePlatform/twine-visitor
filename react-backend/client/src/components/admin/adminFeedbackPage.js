9ab205d568d612756eb06d0ff83711725be8ed09
AstroAsh <astroaisha@gmail.com>
2018-03-20 14:32:51 +0200

    👷‍♀️ Test chart displaying

--- react-backend/client/src/components/admin/adminFeedbackPage.js
+++ react-backend/client/src/components/admin/adminFeedbackPage.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Pie } from 'react-chartjs-2';
 import Logoutbutton from '../visitors/logoutbutton';
 
+const data = {
+  labels: ['Red', 'Green', 'Yellow'],
+  datasets: [
+    {
+      data: [300, 50, 100],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+    },
+  ],
+};
 export default class AdminFeedbackPage extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <div>
         <h1>Feedback</h1>
+        <br />
+        <Pie data={data} />
+        <br />
         <Link to="/" onClick={this.removeAdmin}>
           <button className="Button ButtonBack">Back to the main page</button>
         </Link>
