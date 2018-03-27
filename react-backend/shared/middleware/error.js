7363789056c2a28b26a7a52852e07ec603f0d8c7
Elias Malik <elias0789@gmail.com>
2018-03-27 19:23:40 +0100

    ➕ add Boom and add Boom handling to error middleware
    Boom is a utility library to make handling server-side
    errors easier. We should now no longer be manually
    setting the response in the case of an error in handlers.
    Instead we should instantiate a Boom object and call `next`
    with it. This will then be handled by the error
    middleware.

--- react-backend/shared/middleware/error.js
+++ react-backend/shared/middleware/error.js
@@ -3,6 +3,8 @@
  *
  * To be mounted on the root app
  */
+const Boom = require('boom');
+
 
 // catch 404 and forward to error handler
 exports.notFound = (req, res, next) => {
@@ -12,7 +14,13 @@ exports.notFound = (req, res, next) => {
 };
 
 // error handler
-exports.errorHandler = (err, req, res) => {
+exports.errorHandler = (err, req, res, next) => { // eslint-disable-line
+  if (Boom.isBoom(err)) {
+    const { statusCode, message } = err.output.payload;
+
+    return res.status(statusCode).send({ result: null, error: message });
+  }
+
   if (err === 'notauthorized') {
     return res.status(401).send({ error: 'Not logged in' });
   }
@@ -21,6 +29,5 @@ exports.errorHandler = (err, req, res) => {
   const error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.send({ error, message });
+  res.status(err.status || 500).send({ error, message });
 };
