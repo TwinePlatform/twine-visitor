df6317db00dcda4acc88b2292c5cce24020c2986
Elias Malik <elias0789@gmail.com>
2018-04-03 23:45:05 +0100

    🎨 update API and DB functions for activities
    * Allow updates to be partial
    * Inserts return all fields
    * API responses follow standard form
    * Update tests

--- react-backend/database/queries/activity_add.js
+++ react-backend/database/queries/activity_add.js
@@ -1,11 +1,11 @@
 const insertInfo = `
 INSERT INTO activities (name, cb_id) VALUES ($1, $2)
-RETURNING id`;
+RETURNING *`;
 
 const insertActivity = (dbConnection, name, cbId) => {
   if (!name || !cbId) return Promise.reject(new Error('Bad query arguments'));
 
-  return dbConnection.query(insertInfo, [name, cbId]).then(res => res.rows[0].id);
+  return dbConnection.query(insertInfo, [name, cbId]).then(res => res.rows[0]);
 };
 
 module.exports = insertActivity;
