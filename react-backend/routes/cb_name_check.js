37af76b4db1fdd5cfe730c967e5e228af4ad5698
James <jinglis12@googlemail.com>
2018-04-05 21:18:02 +0100

    Finished adding org name route for visitor level authentication. #274

--- react-backend/routes/cb_name_check.js
+++ react-backend/routes/cb_name_check.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const cbDetails = require('../database/queries/cb/cb_details');
 
-router.post('/', (req, res, next) => {
-  console.log('Hello?!');
+router.get('/', (req, res, next) => {
   cbDetails(req.app.get('client:psql'), req.auth.cb_id)
     .then(details => res.send({ result: details }))
     .catch(next);
