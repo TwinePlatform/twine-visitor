b5a465b56edba900f536ffa6e04464160361a3be
Elias Malik <elias0789@gmail.com>
2018-04-06 15:35:31 +0100

    🐛 Fix crappy validation errors

---
+++ react-backend/client/src/cb-admin/constants/error_text.js
@@ -0,0 +1,3 @@
+export const PASSWORD_CONFIRMATION_DOESNT_MATCH = 'Password doesn\'t match';
+export const PASSWORD_NOT_STRONG = 'Password isn\'t strong enough: At least 8 characters, with capitals, numbers and symbols';
+export const ORG_NAME_INVALID = 'Organisation name invalid, must be alphanumeric';
