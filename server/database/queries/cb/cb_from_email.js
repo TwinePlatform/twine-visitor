9dc7d78fcf84de4adbe9473310bc08cc5c7f8b26
Elias Malik <elias0789@gmail.com>
2018-04-11 00:52:05 +0100

    🔥 remove dead files and lines

--- server/database/queries/cb/cb_from_email.js
+++ server/database/queries/cb/cb_from_email.js
@@ -2,16 +2,7 @@ const getCBQuery =
   'SELECT id, org_name, uploadedFileCloudinaryUrl FROM cbusiness WHERE email=$1';
 
 const getCBFromEmail = (dbConnection, email) =>
-  new Promise((resolve, reject) => {
-    dbConnection
-      .query(getCBQuery, [email])
-      .then(res => {
-        if (res.rowCount) {
-          return resolve(res.rows[0]);
-        }
-        reject('no results found');
-      })
-      .catch(reject);
-  });
+  dbConnection.query(getCBQuery, [email])
+    .then((res) => res.rows[0] || null);
 
 module.exports = getCBFromEmail;
--- server/functions/cbhash.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = (secret, pswd) => {
-  const hash = crypto
-    .createHmac('sha256', secret)
-    .update(pswd)
-    .digest('hex');
-  return hash;
-};
