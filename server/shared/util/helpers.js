8f40a0d3718c45544d5819bcf1a1d9ac9b347ac9
AstroAsh <astroaisha@gmail.com>
2018-04-23 22:21:05 +0300

    λ Add type annotation for helper funcs

--- server/shared/util/helpers.js
+++ server/shared/util/helpers.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const { assoc, curry, reduce, keys } = require('ramda');
 
+// ageString :: String -> [ Int ]
 const ageRange = ageString =>
   ageString
     .replace('+', '')
@@ -8,7 +9,9 @@ const ageRange = ageString =>
     .map(x => moment().year() - x)
     .reverse();
 
+// renameKeys :: {a: b} -> {a: *} -> {b: *}
 const renameKeys = curry((keysMap, obj) =>
   reduce((acc, key) => assoc(keysMap[key] || key, obj[key], acc), {}, keys(obj))
 );
+
 module.exports = { ageRange, renameKeys };
