5f4f464b8bccc9b63a9e3318b494bb28e2165501
James <jinglis12@googlemail.com>
2018-04-25 20:46:10 +0100

    Fixed useless test and changed imported variables to use correct case.
    Fix package-lock

--- server/routes/__tests__/export_visits.test.js
+++ server/routes/__tests__/export_visits.test.js
@@ -3,17 +3,29 @@ const request = require('supertest');
 const jwt = require('jsonwebtoken');
 const createApp = require('../../app');
 const { getConfig } = require('../../../config');
+const { refresh: refreshDB } = require('../../../db/scripts');
 
 const config = getConfig(process.env.NODE_ENV);
 
-test('POST /api/cb/export | Correct token', t => {
+test('POST /api/cb/export | Correct query data received', async t => {
   const app = createApp(config);
   const secret = app.get('cfg').session.cb_admin_jwt_secret;
 
   const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, secret);
+  await refreshDB();
 
-  const payload = {
+  const payload = {};
+
+  const expected = {
+    visit_id: 2,
+    visitor_id: 2,
+    visitor_name: 'britney spears',
+    gender: 'female',
+    yob: 1982,
+    activity: 'Yoga',
+    visit_date: '2018-03-20T13:32:30.000Z',
   };
+
   request(app)
     .post('/api/cb/export')
     .set('authorization', token)
@@ -23,27 +35,7 @@ test('POST /api/cb/export | Correct token', t => {
     .end((err, res) => {
       t.notOk(err, err || 'Passes supertest expect criteria');
       t.ok(res.body.result, 'Res includes result object');
-      t.end();
-    });
-});
-
-test('POST /api/cb/export | Incorrect Token', t => {
-  const app = createApp(config);
-  const secret = app.get('cfg').session.standard_jwt_secret;
-
-  const token = jwt.sign({ email: 'jinglis12@googlemail.com' }, secret);
-
-  const payload = {
-  };
-  request(app)
-    .post('/api/cb/export')
-    .set('authorization', token)
-    .send(payload)
-    .expect(401)
-    .expect('Content-Type', /json/)
-    .end((err, res) => {
-      t.notOk(err, err || 'Passes supertest expect criteria');
-      t.notOk(res.body.result, 'Res does not include results object');
+      t.deepEquals(res.body.result[0], expected, 'Expected object returned');
       t.end();
     });
 });
