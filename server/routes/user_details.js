a53ea5f0c8752cc042dd2d5ccc6ed7727ae82e5f
Elias Malik <elias0789@gmail.com>
2018-04-30 17:08:46 +0100

    🐛 update function signature after changes
    We weren't passing the new options object, so the
    route was just returning details of the first row
    in the table

--- server/routes/user_details.js
+++ server/routes/user_details.js
@@ -3,7 +3,7 @@ const userDetails = require('../database/queries/user_details');
 const qrCodeMaker = require('../functions/qrcodemaker');
 
 router.post('/', (req, res, next) => {
-  userDetails(req.app.get('client:psql'), req.auth.cb_id, req.body.userId)
+  userDetails(req.app.get('client:psql'), { where: { cb_id: req.auth.cb_id, id: req.body.userId } })
     .then(({ hash, ...details }) => {
       qrCodeMaker(hash)
         .then((qrCode) => res.send({ result: { qrCode, ...details} }));
