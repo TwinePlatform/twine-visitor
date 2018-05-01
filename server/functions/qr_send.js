472ab2fb8cfa0de75f34373246c07ce9d3b7cdce
Elias Malik <elias0789@gmail.com>
2018-05-01 10:33:04 +0100

    🔧 remove CB_EMAIL from sendemail
    * Change signature to accept community businesses email
    * Fix call signature in all callers
    
    Related to #396

--- server/functions/qr_send.js
+++ server/functions/qr_send.js
@@ -2,12 +2,12 @@ const qrCodeMaker = require('../functions/qrcodemaker');
 const generatePdf = require('../functions/pdfgenerator');
 const sendemail = require('../functions/sendemail');
 
-const sendQrCode = (client, email, name, hash, cbLogo) => {
-  if (!email) return console.log('Missing the person to deliver this email to');
+const sendQrCode = (client, visitorEmail, cbEmail, name, hash, cbLogo) => {
+  if (!visitorEmail) return console.log('Missing the person to deliver this email to');
 
   qrCodeMaker(hash)
     .then(QRcodeBase64Url => generatePdf(QRcodeBase64Url, cbLogo))
-    .then(pdf => sendemail(client, email, name, pdf))
+    .then(pdf => sendemail(client, visitorEmail, cbEmail, name, pdf))
     .catch(err => {
       console.log('Failed to send email ', err);
     });
--- server/routes/qr_user_send.js
+++ server/routes/qr_user_send.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res, next) => {
     const user = await getUserDetails(pgClient, { where: { cb_id: req.auth.cb_id, id: req.body.id } });
     const QRcodeBase64Url = await qrCodeMaker(user.hash);
     const pdf = await generatePdf(QRcodeBase64Url, req.auth.cb_logo);
-    await sendEmail(pmClient, req.body.email, req.body.name, pdf);
+    await sendEmail(pmClient, req.body.email, req.auth.cb_email, req.body.name, pdf);
 
     res.send({ result: null });
 
