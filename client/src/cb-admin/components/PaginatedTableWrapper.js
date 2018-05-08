fd9d65c70a0a994c2f70cdcbe197727fdf5e8943
Elias Malik <elias0789@gmail.com>
2018-05-08 11:23:21 +0100

    ✨ Add component to wrap tables to make them paginated
    The wrapper requires the table element to be the only child element, and
    requires a function to be passed as a prop which loads subsequent rows
    into the table

---
+++ client/src/cb-admin/components/PaginatedTableWrapper.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Paginate from 'react-paginate';
+
+
+export default class PaginatedTableWrapper extends React.Component {
+  static propTypes = {
+    rowCount: PropTypes.number.isRequired, // eslint-disable-line
+    loadRows: PropTypes.func.isRequired,
+    rowsPerPage: PropTypes.number,
+    children: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.arrayOf(PropTypes.element),
+    ]).isRequired,
+  }
+
+  static defaultProps = {
+    rowsPerPage: 10,
+  }
+
+  static getDerivedStateFromProps(nextProps) {
+    return { totalPages: Math.ceil(nextProps.rowCount / nextProps.rowsPerPage) };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      offset: 0,
+      totalPages: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.loadRows();
+  }
+
+  onPageChange = ({ selected }) =>
+    this.setState(
+      { offset: Math.ceil(selected * this.props.rowsPerPage) },
+      this.loadRows,
+    )
+
+  loadRows = () => this.props.loadRows(this.state.offset)
+
+  render() {
+    const Table = React.Children.only(this.props.children);
+
+    return (
+      <div>
+        {
+          Table
+        }
+        <Paginate
+          pageCount={this.state.totalPages}
+          pageRangeDisplayed={4}
+          marginPagesDisplayed={1}
+          previousLabel="<"
+          nextLabel=">"
+          breakLabel="..."
+          onPageChange={this.onPageChange}
+          breakClassName="rpg-break"
+          containerClassName="rpg-container"
+          pageClassName="rpg-page"
+          pageLinkClassName="rpg-pageLink"
+          activeClassName="rpg-active"
+          previousClassName="rpg-previous"
+          nextClassName="rpg-next"
+          previousLinkClassName="rpg-previousLink"
+          nextLinkClassName="rpg-nextLink"
+          disabledClassName="rpg-disabled"
+        />
+      </div>
+    );
+  }
+}
