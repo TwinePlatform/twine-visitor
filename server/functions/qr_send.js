7f97ccb40b665b53ac5ef91be85539952f1d8ece
Elias Malik <elias0789@gmail.com>
2018-05-30 16:24:54 +0100

    💬 add CB name to email template model object
    Postmark template already updated to accept new parameter.
    
    Related to #432

--- server/functions/qr_send.js
+++ server/functions/qr_send.js
@@ -2,12 +2,12 @@ const qrCodeMaker = require('../functions/qrcodemaker');
 const generatePdf = require('../functions/pdfgenerator');
 const sendemail = require('../functions/sendemail');
 
-const sendQrCode = (client, visitorEmail, cbEmail, name, hash, cbLogo) => {
+const sendQrCode = (client, visitorEmail, cbEmail, name, hash, cbName, cbLogo) => {
   if (!visitorEmail) return console.log('Missing the person to deliver this email to');
 
   qrCodeMaker(hash)
     .then(QRcodeBase64Url => generatePdf(QRcodeBase64Url, cbLogo))
-    .then(pdf => sendemail(client, visitorEmail, cbEmail, name, pdf))
+    .then(pdf => sendemail(client, visitorEmail, cbEmail, name, cbName, pdf))
     .catch(err => {
       console.log('Failed to send email ', err);
     });
