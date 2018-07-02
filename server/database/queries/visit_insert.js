94da2e67756a44a3ab901fadd35b613650ded721
Elias Malik <elias0789@gmail.com>
2018-07-02 17:03:13 +0100

    🐛 set correct foreign key when inserting visit record
    Previously we were just getting the first activity record matching on
    name _only_. We need to match on CB ID too.
    
    Related to #440

--- server/database/queries/visit_insert.js
+++ server/database/queries/visit_insert.js
@@ -2,7 +2,7 @@ const insertVisit = `
    INSERT INTO visits (usersId, activitiesId, cb_id, date)
    VALUES (
      (SELECT id FROM users WHERE hash = $1),
-     (SELECT id FROM activities WHERE name = $2 LIMIT 1),
+     (SELECT id FROM activities WHERE name = $2 AND cb_id = $3 LIMIT 1),
      $3,
      DEFAULT)`;
 
