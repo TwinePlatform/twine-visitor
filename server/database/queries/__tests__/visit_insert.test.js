0e139dee3e7f41b0c85a989c6c9732a7d9a06f3b
Elias Malik <elias0789@gmail.com>
2018-07-02 17:03:13 +0100

    🐛 set correct foreign key when inserting visit record
    Previously we were just getting the first activity record matching on
    name _only_. We need to match on CB ID too.
    
    Related to #440

--- server/database/queries/__tests__/visit_insert.test.js
+++ server/database/queries/__tests__/visit_insert.test.js
@@ -30,7 +30,50 @@ test('DB Query | visit_insert', async (tape) => {
       t.equals(
         actual.rows.length,
         oldCount.rows.length + 1,
-        'insertVisit adds new activity with correct hash, activity name and cb id'
+        'insertVisit adds new visit'
+      );
+
+      t.end();
+    } catch (error) {
+      t.end(error);
+    }
+  });
+
+  tape.test('visit_insert | duplicate name', async (t) => {
+    try {
+      await refreshDB();
+      await client.query(
+        `INSERT INTO activities
+          (name, cb_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+        VALUES
+          ($1, $2, $3, $3, $3, $3, $3, $3, $3)`,
+        ['Swimming', 1, true]
+      );
+
+      const oldCount = await client.query(
+        'SELECT * FROM visits WHERE usersId = 1 AND activitiesId = 7'
+      );
+
+      await insertVisit(
+        client,
+        '9fb59d630d2fb12f7478c56c5f1b2fff20e0dd7c9d3a260eee7308a8eb6cd955',
+        'Swimming',
+        1
+      );
+
+      const actual = await client.query(
+        'SELECT * FROM visits WHERE usersId = 1 AND activitiesId = 7'
+      );
+      const newVisit = await client.query('SELECT EXISTS (SELECT * FROM visits WHERE usersid=$1 AND cb_id=$2)', [1, 1]);
+
+      t.equals(
+        actual.rows.length,
+        oldCount.rows.length + 1,
+        'insertVisit adds new visit'
+      );
+      t.ok(
+        newVisit.rows[0].exists,
+        'Newly inserted visit registered against correct CB, despite duplicate name'
       );
 
       t.end();
