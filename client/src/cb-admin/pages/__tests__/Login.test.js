57483b83146ca6a76b7f0a47ef38cf27e8c2df7e
AstroAsh <astroaisha@gmail.com>
2018-07-26 10:17:22 -0400

    ✅ Update Login tests to submit accurate data

--- client/src/cb-admin/pages/__tests__/Login.test.js
+++ client/src/cb-admin/pages/__tests__/Login.test.js
@@ -13,28 +13,40 @@ import Login from '../Login';
 describe('Login Component', () => {
   afterEach(cleanup);
 
-  test(':: error response details displays error message', async () => {
+  test(':: incorrect user details returns 401 and displays error message', async () => {
     const mock = new MockAdapter(axios);
     mock.onPost('/api/cb/login')
       .reply(401, { result: null, error: 'Credentials not recognised' });
 
-    const { getByText } = renderWithRouter({ setLoggedIn: () => { } })(Login);
+    const { getByText, getByLabelText } = renderWithRouter({ setLoggedIn: () => { } })(Login);
+    const email = getByLabelText('Email')
+    const password = getByLabelText('Password')
     const submit = getByText('LOGIN');
+    email.value = '123@hi.com';
+    password.value = 'lolLOL123'
+    fireEvent.change(email);
+    fireEvent.change(password);
     fireEvent.click(submit);
 
     const error = await waitForElement(() => getByText('Credentials', { exact: false }));
     expect(error.textContent).toEqual('Credentials not recognised');
   });
 
-  test(':: correct response redirects to homepage', async () => {
+  test(':: correct user details returns 200 and redirects to homepage', async () => {
     const mock = new MockAdapter(axios);
     mock.onPost('/api/cb/login')
       .reply(200, {});
 
-    const { getByText, history } =
+    const { getByText, history, getByLabelText } =
       renderWithRouter({ setLoggedIn: () => { }, route: '/cb/login' })(Login);
 
+    const email = getByLabelText('Email')
+    const password = getByLabelText('Password')
     const submit = getByText('LOGIN');
+    email.value = 'findmyfroggy@frogfinders.com';
+    password.value = 'Funnyfingers11!'
+    fireEvent.change(email);
+    fireEvent.change(password);
     fireEvent.click(submit);
 
     await wait(() => {
