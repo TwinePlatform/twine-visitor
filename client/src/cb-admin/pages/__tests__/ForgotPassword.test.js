55299a50d22889e42a8fd9c4a1a92fd0de3998b5
AstroAsh <astroaisha@gmail.com>
2018-07-27 11:51:05 -0400

    ✅ Add tests for ForgotPassword component

---
+++ client/src/cb-admin/pages/__tests__/ForgotPassword.test.js
@@ -0,0 +1,64 @@
+import {
+  fireEvent,
+  cleanup,
+  waitForElement,
+  wait,
+} from 'react-testing-library';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import renderWithRouter from '../../../tests';
+import ForgotPassword from '../ForgotPassword';
+
+
+describe('Login Component', () => {
+  let mock;
+
+  beforeAll(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(cleanup);
+
+  test(':: subscribed email sends 200 and redirects to login', async () => {
+    expect.assertions(1);
+
+    mock.onPost('/api/cb/pwd/reset')
+      .reply(200, { result: null });
+
+    const { getByText, getByLabelText, history } =
+      renderWithRouter({ setLoggedIn: () => { } })(ForgotPassword);
+
+    const email = getByLabelText('Email');
+    const submit = getByText('CONTINUE');
+
+    email.value = 'frogfindmyfroggy@frogfinders.com';
+    fireEvent.change(email);
+    fireEvent.click(submit);
+
+    await wait(() => history.length === 2);
+    expect(history.location.pathname).toEqual('/cb/login');
+  });
+
+  test(':: unsubscribed email sends 401 and displays error message', async () => {
+    expect.assertions(1);
+
+    mock.onPost('/api/cb/pwd/reset')
+      .reply(401, { result: null, error: 'Email not recognised' });
+
+    const { getByText, getByLabelText } =
+      renderWithRouter({ setLoggedIn: () => { } })(ForgotPassword);
+
+    const email = getByLabelText('Email');
+    const submit = getByText('CONTINUE');
+
+    email.value = 'notreal@frogfinders.com';
+    fireEvent.change(email);
+    fireEvent.click(submit);
+
+    const error = await waitForElement(() => getByText('Email not recognised'));
+    expect(error.textContent).toEqual('Email not recognised');
+  });
+
+
+});
+
