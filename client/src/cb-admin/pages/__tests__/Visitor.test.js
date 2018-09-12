c2c56853b3792f97b41c5c7d9029c8062e599b29
Elias Malik <elias0789@gmail.com>
2018-09-12 14:09:07 +0100

    ✅ add tests for Visitor update feature

--- client/src/cb-admin/pages/__tests__/Visitor.test.js
+++ client/src/cb-admin/pages/__tests__/Visitor.test.js
@@ -2,6 +2,7 @@ import {
   cleanup,
   waitForElement,
   wait,
+  fireEvent,
 } from 'react-testing-library';
 import MockAdapter from 'axios-mock-adapter';
 import { axios } from '../../../api';
@@ -30,10 +31,10 @@ describe('Visitor Component', () => {
           gender: 'female',
           birthYear: 1998,
           email: 'maemail@gmail.com',
-          phone_number: '7835110026',
-          registered_at: '2017-05-15T12:24:52.000Z',
-          email_consent: false,
-          sms_consent: false } });
+          phoneNumber: '7835110026',
+          createdAt: '2017-05-15T12:24:52.000Z',
+          isEmailContactConsentGranted: false,
+          isSMSContactConsentGranted: false } });
 
     mock.onGet('/community-businesses/me')
       .reply(200,
@@ -86,6 +87,140 @@ describe('Visitor Component', () => {
     })(Visitor);
 
     await wait(() => expect(history.location.pathname).toEqual('/admin/login'));
+  });
+
+  test(':: can update visitor details', async () => {
+    expect.assertions(3);
+
+    mock
+      .onGet('/community-businesses/me/visitors/4')
+      .reply(200,
+        { result: {
+          qrCode: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgEQVR4AewaftIAAAzUSU==',
+          id: 4,
+          name: 'yusra mardini',
+          gender: 'female',
+          birthYear: 1998,
+          email: 'maemail@gmail.com',
+          phoneNumber: '7835110026',
+          createdAt: '2017-05-15T12:24:52.000Z',
+          isEmailContactConsentGranted: false,
+          isSMSContactConsentGranted: false } })
+      .onPut('/community-businesses/me/visitors/4')
+      .reply(200, {
+        result: {
+          qrCode: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgEQVR4AewaftIAAAzUSU==',
+          id: 4,
+          name: 'foosra mardini',
+          gender: 'male',
+          birthYear: 1998,
+          email: 'manewmail@gmail.com',
+          phoneNumber: '7835110026',
+          createdAt: '2017-05-15T12:24:52.000Z',
+          modifiedAt: (new Date()).toISOString(),
+          isEmailContactConsentGranted: false,
+          isSMSContactConsentGranted: false,
+        },
+      });
+
+    mock
+      .onGet('/community-businesses/me')
+      .reply(200,
+        { result: {
+          id: 3,
+          name: 'Frog Finders',
+          sector: 'Environment or nature',
+          logoUrl: null,
+          createdAt: '2017-05-15T12:24:56.000Z' } })
+      .onGet('/users/me')
+      .reply(200, { email: 'findmyfroggy@frogfinders.com' });
+
+    const tools = renderWithRouter({
+      match: {
+        isExact: true,
+        params: { id: '4' },
+        path: '/cb/visitors/:id',
+        url: '/cb/visitors/4',
+      },
+    })(Visitor);
+
+    const [nameInput, emailInput, genderInput, submitBtn] = await waitForElement(() => [
+      tools.getByLabelText('Name'),
+      tools.getByLabelText('Email'),
+      tools.getByLabelText('Gender'),
+      tools.getByText('SAVE'),
+    ]);
+
+    fireEvent.change(nameInput, { target: { value: 'foosra mardini' } });
+    fireEvent.change(emailInput, { target: { value: 'manewmail@gmail.com' } });
+    fireEvent.change(genderInput, { target: { value: 'male' } });
+    fireEvent.click(submitBtn);
+
+    const [nameText, emailText, genderText] = await waitForElement(() => [
+      tools.getByText('foosra mardini'),
+      tools.getByText('manewmail@gmail.com'),
+      tools.getByText('male'),
+    ]);
+
+    expect(nameText).toBeTruthy();
+    expect(emailText).toBeTruthy();
+    expect(genderText).toBeTruthy();
+  });
+
+  test(':: fail to update visitor details', async () => {
+    expect.assertions(1);
+
+    mock
+      .onGet('/community-businesses/me/visitors/4')
+      .reply(200,
+        { result: {
+          qrCode: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgEQVR4AewaftIAAAzUSU==',
+          id: 4,
+          name: 'yusra mardini',
+          gender: 'female',
+          birthYear: 1998,
+          email: 'maemail@gmail.com',
+          phoneNumber: '7835110026',
+          createdAt: '2017-05-15T12:24:52.000Z',
+          isEmailContactConsentGranted: false,
+          isSMSContactConsentGranted: false } })
+      .onPut('/community-businesses/me/visitors/4')
+      .reply(400, { error: { email: 'Invalid email' } });
+
+    mock
+      .onGet('/community-businesses/me')
+      .reply(200,
+        { result: {
+          id: 3,
+          name: 'Frog Finders',
+          sector: 'Environment or nature',
+          logoUrl: null,
+          createdAt: '2017-05-15T12:24:56.000Z' } })
+      .onGet('/users/me')
+      .reply(200, { email: 'findmyfroggy@frogfinders.com' });
+
+    const tools = renderWithRouter({
+      match: {
+        isExact: true,
+        params: { id: '4' },
+        path: '/cb/visitors/:id',
+        url: '/cb/visitors/4',
+      },
+    })(Visitor);
+
+    const [birthYearInput, submitBtn] = await waitForElement(() => [
+      tools.getByLabelText('Email'),
+      tools.getByText('SAVE'),
+    ]);
+
+    fireEvent.change(birthYearInput, { target: { value: 'qdeoifwrgheui' } });
+    fireEvent.click(submitBtn);
+
+    const [errorText] = await waitForElement(() => [
+      tools.getByText('Invalid email'),
+    ]);
+
+    expect(errorText).toBeTruthy();
   });
 });
 
