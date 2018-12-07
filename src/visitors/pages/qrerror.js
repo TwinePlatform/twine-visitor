beabeb7098df564ff6afbd1040c9b239c3328681
Elias Malik <elias0789@gmail.com>
2018-12-07 13:06:12 +0000

    🚸 improve UX of QR error page by passing error message in qs
    * Make buttons consistent with rest of app's style
    * Display error message if passed through the query string
    * If no user found, display a message saying so

--- src/visitors/pages/qrerror.js
+++ src/visitors/pages/qrerror.js
@@ -1,21 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import qs from 'qs';
 import QRPrivacy from '../components/qrprivacy';
+import { PrimaryButton } from '../../shared/components/form/base';
 
-export default () => (
-  <div className="row">
-    <section className="Main col-9">
-      <h1>We’re sorry, there was a problem scanning your code</h1>
-      <h2>Please choose one of these options or go to Reception for help</h2>
-      <Link to="/visitor/signup">
-        <button className="Button">Register as a new user</button>
-      </Link>
-      <br />
-      <Link to="/visitor/login">
-        <button className="Button">Try to scan QR code again</button>
-      </Link>
-      <br />
-    </section>
-    <QRPrivacy className="col-3" />
-  </div>
-);
+const Button = styled(PrimaryButton)`
+  padding: 1em 0.5em;
+  margin: 1em;
+`;
+
+const ErrorText = {
+  no_user: 'Your QR code does not correspond to a user at this organisation',
+  default: null,
+};
+
+const QrError = (props) => {
+  const query = qs.parse(props.history.location.search.slice(1) || '');
+
+  return (
+    <div className="row">
+      <section>
+        <h1>{props.headerContent}</h1>
+        <h2>{ErrorText[query.e || 'default']}</h2>
+        <h4>{props.subHeaderContent}</h4>
+        <Link to="/visitor/signup">
+          <Button>Register as a new user</Button>
+        </Link>
+        <Link to="/visitor/login">
+          <Button>Try to scan QR code again</Button>
+        </Link>
+      </section>
+      <QRPrivacy />
+    </div>
+  )
+  ;
+};
+
+QrError.propTypes = {
+  headerContent: PropTypes.string,
+  subHeaderContent: PropTypes.string,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      search: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+QrError.defaultProps = {
+  headerContent: 'We’re sorry, there was a problem scanning your code',
+  subHeaderContent: 'Please choose one of these options or go to Reception for help',
+};
+
+export default QrError;
