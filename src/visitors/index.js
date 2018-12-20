b066ea628ecf70c32c282fdb7feb0a438a5c4bbe
Elias Malik <elias0789@gmail.com>
2018-12-20 23:54:41 +0000

    ♻️ Split most routes into two nested components
    One under `/cb/*` and one under `/visitor/*`

---
+++ src/visitors/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './pages/homeVisitor';
+import Signup from './pages/Signup';
+import Thanks from './pages/thanks';
+import ThanksFeedback from './pages/thank_you_feedback';
+import QrError from './pages/qrerror';
+import Login from './pages/Login';
+import redirectAfterTimeout from '../shared/components/hoc/redirect_after_timeout';
+
+
+export default () => (
+  <>
+    <Route exact path="/visitor/home" component={Home} />
+    <Route exact path="/visitor/signup" component={Signup} />
+    <Route exact path="/visitor/signup/*" component={Signup} />
+    <Route exact path="/visitor/login" component={Login} />
+    <Route exact path="/visitor/qrerror" component={QrError} />
+    <Route exact path="/visitor/end" component={redirectAfterTimeout('/visitor/home', 5000)(Thanks)} />
+    <Route exact path="/visitor/thankyou" component={redirectAfterTimeout('/visitor/home', 5000)(ThanksFeedback)} />
+  </>
+);
