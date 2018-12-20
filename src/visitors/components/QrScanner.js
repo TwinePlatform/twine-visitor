dbd5500d0debadb5b92d921629bb9d7ec9cc0f8b
Elias Malik <elias0789@gmail.com>
2018-12-20 23:40:15 +0000

    ♻️ refactor out QR scanning into own component

---
+++ src/visitors/components/QrScanner.js
@@ -0,0 +1,101 @@
+/* global Instascan */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Paragraph } from '../../shared/components/text/base';
+
+
+const Video = styled.video`
+  background-color: black;
+  border-radius: 0.2em;
+  width: 100%;
+`;
+
+
+const instascanAvailable = () => {
+  try {
+    return Boolean(Instascan.Scanner);
+  } catch (error) {
+    return false;
+  }
+};
+
+
+export default class QrScanner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasScanned: false,
+      errors: {},
+    };
+
+    this.playbackRef = null;
+    this.scanner = null;
+
+    this.setPlaybackRef = (element) => {
+      this.playbackRef = element;
+    };
+  }
+
+  componentDidMount() {
+    if (!instascanAvailable()) {
+      this.props.onScannerError(new Error('Instascan is not available'));
+      return;
+    }
+
+    this.scanner =
+      this.scanner || new Instascan.Scanner({ video: this.playbackRef, scanPeriod: 5 });
+
+    Instascan.Camera.getCameras()
+      .then((cameras) => {
+        if (cameras.length < 1) {
+          throw new Error('No accessible cameras');
+        }
+        return this.scanner.start(cameras[0]);
+      })
+      .catch(error => this.props.onCameraError(error));
+
+    if (!this.state.hasScanned) {
+      this.scanner.addListener('scan', this.scanListener);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.scanner) {
+      this.scanner.stop()
+        .then(() => {
+          this.scanner = null;
+        })
+        .catch(error => this.props.onScannerError(error));
+
+      this.scanner.removeListener('scan', this.scanListener);
+    }
+  }
+
+  scanListener = (content) => {
+    this.scanner.stop();
+    this.setState({ hasScanned: true });
+    this.props.onScan(content);
+  }
+
+  render() {
+    return (
+      <div id="qr-scanner-container">
+        <Paragraph>Scan QR code</Paragraph>
+        <Video ref={this.setPlaybackRef} />
+      </div>
+    );
+  }
+}
+
+QrScanner.propTypes = {
+  onCameraError: PropTypes.func,
+  onScannerError: PropTypes.func,
+  onScan: PropTypes.func,
+};
+
+QrScanner.defaultProps = {
+  onCameraError: () => {},
+  onScannerError: () => {},
+  onScan: () => {},
+};
