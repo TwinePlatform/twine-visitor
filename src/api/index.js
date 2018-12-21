9197cd6613c2d25e5c706ebc15e1d54e6f29903f
AstroAsh <astroaisha@gmail.com>
2018-12-21 13:11:00 +0000

    🐛 fix proxy config
    - build base url dependant on wether HOST_DOMAIN is set
    - remove HOST_DOMAIN from required env vars in heroku config

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,9 @@ import _axios, { create } from 'axios';
 import { pathOr, equals, compose } from 'ramda';
 import qs from 'qs';
 
-const baseURL = `${process.env.REACT_APP_API_HOST_DOMAIN}/v1`;
+const baseURL = process.env.REACT_APP_API_HOST_DOMAIN ?
+  `${process.env.REACT_APP_API_HOST_DOMAIN}/v1`
+  : '/v1';
 
 export const axios = create({
   baseURL,
