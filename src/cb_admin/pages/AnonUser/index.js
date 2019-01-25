969a5e8fb84ec8ca03151231fe86a67ada731fbe
AstroAsh <astroaisha@gmail.com>
2019-01-25 12:35:25 +0100

    ♻️ add validation error messages on failed response

--- src/cb_admin/pages/AnonUser/index.js
+++ src/cb_admin/pages/AnonUser/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { CommunityBusiness, Visitors } from '../../../api';
+import { CommunityBusiness, Visitors, ErrorUtils } from '../../../api';
 import { redirectOnError, renameKeys } from '../../../util';
-import { FlexContainerCol } from '../../../shared/components/layout/base';
 import CreateAnonUserForm from './CreateAnonUserForm';
 import PrintOption from './PrintOption';
 
@@ -16,7 +15,7 @@ const stage = {
 export default class AnonUser extends Component {
 
   state = {
-    name: 'ANON_',
+    name: 'Anonymous ',
     genders: [],
     errors: {},
     stage: stage.ONE,
@@ -30,7 +29,6 @@ export default class AnonUser extends Component {
 
         return Promise.all([getCb, getGenders]);
       })
-      // .then(console.log)
       .then(([{ data: { result: cbRes } }, { data: { result: gendersRes } }]) =>
         this.setState({
           cbOrgName: cbRes.name,
@@ -57,6 +55,13 @@ export default class AnonUser extends Component {
           stage: stage.TWO,
           qrCode: res.data.result.qrCode,
         });
+      })
+      .catch((err) => {
+        if (ErrorUtils.errorStatusEquals(err, 400)) {
+          this.setState({ errors: ErrorUtils.getValidationErrors(err) });
+        } else {
+          redirectOnError(this.props.history.push, err);
+        }
       });
   }
 
@@ -65,14 +70,13 @@ export default class AnonUser extends Component {
     this.setState({ [name]: e.target.value });
   }
 
-  printOption = (<FlexContainerCol />)
-
-  render() {
+  render() { // eslint-disable-line consistent-return
     const { state } = this;
 
-    switch (state.stage) {
+    switch (state.stage) { // eslint-disable-line default-case
       case stage.ONE:
         return (<CreateAnonUserForm
+          name={state.name}
           handleChange={this.handleChange}
           onSubmit={this.onSubmit}
           errors={this.state.errors}
@@ -84,12 +88,9 @@ export default class AnonUser extends Component {
           qrCode={state.qrCode}
           cbLogoUrl={state.cbLogoUrl}
         />);
-      default:
-        return this.createUserForm; // necessary as js is useless
     }
   }
 }
-
 
 AnonUser.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
