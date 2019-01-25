e089fe3e0807f223e7bee2cd50737475599158b1
Elias Malik <elias0789@gmail.com>
2019-01-25 10:50:41 +0000

    ♻️ add index file to form component dir
    For easier imports

---
+++ src/shared/components/form/index.js
@@ -0,0 +1,40 @@
+import {
+  Input,
+  Select,
+  SelectWrapper,
+  Option,
+  Label,
+  Button,
+  PrimaryButton,
+  PrimaryButtonNoFill,
+  SecondaryButton,
+  SecondaryButtonNoFill,
+  Form,
+  FormSection,
+} from './base';
+import DottedButton from './DottedButton';
+import LabelledInput from './LabelledInput';
+import LabelledSelect from './LabelledSelect';
+import StyledLabelledCheckbox from './StyledLabelledCheckbox';
+import withValidation from './withValidation';
+
+
+export {
+  Input,
+  Select,
+  SelectWrapper,
+  Option,
+  Label,
+  Button,
+  PrimaryButton,
+  PrimaryButtonNoFill,
+  SecondaryButton,
+  SecondaryButtonNoFill,
+  Form,
+  FormSection,
+  DottedButton,
+  LabelledInput,
+  LabelledSelect,
+  StyledLabelledCheckbox,
+  withValidation,
+};
