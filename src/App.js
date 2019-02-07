da80ad9aebc7e14b92cc27b0f26a2494d5b2381c
AstroAsh <astroaisha@gmail.com>
2019-02-07 16:58:51 +0100

    ♻️ cookie HOC routing
    - on load of site check auth and display loader whilst waiting
    - when authorised dish out all private routes

--- src/App.js
+++ src/App.js
@@ -10,29 +10,30 @@ import HomePage from './shared/pages/Home';
 
 import VisitorRoutes from './visitors';
 import CbAdminRoutes from './cb_admin';
+import Login from './cb_admin/pages/Login';
+import PrivateCookieRoute from './auth/PrivateCookieRoute';
 
 
-export default class App extends Component {
+const ProtectedRoutes = () => (
+  <Switch>
+    <Route exact path="/" component={HomePage} />
+    <Route path="/visitor/*" component={VisitorRoutes} />
+    <Route path="/admin" component={CbAdminRoutes} />
 
-  componentWillMount() {
-    // avoid eslint errors to keep this a class component
-  }
+    <Route exact path="/error/404" component={NotFound} />
+    <Route exact path="/error/:code" component={InternalServerError} />
+    <Route component={NotFound} />
+  </Switch>);
 
-  render() {
-    return (
-      <Dots>
-        <Container>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/visitor/*" component={VisitorRoutes} />
-            <Route path="/cb/*" component={CbAdminRoutes} />
+const App = () => (
+  <Dots>
+    <Container>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <PrivateCookieRoute path="/*" component={ProtectedRoutes} />
+      </Switch>
+    </Container>
+  </Dots>
+);
 
-            <Route exact path="/error/404" component={NotFound} />
-            <Route exact path="/error/:code" component={InternalServerError} />
-            <Route component={NotFound} />
-          </Switch>
-        </Container>
-      </Dots>
-    );
-  }
-}
+export default App;
---
+++ src/auth/PrivateCookieRoute.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { BeatLoader as Bl } from 'react-spinners';
+import styled from 'styled-components';
+
+import { colors } from '../shared/style_guide';
+import { Roles } from '../api';
+
+const status = {
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  FAILURE: 'FAILURE',
+};
+
+const BeatLoader = styled(Bl)`
+  width: 60px;
+  margin: 0 auto;
+  padding-top: 5rem;
+`;
+
+export default class PrivateCookieRoute extends React.Component {
+
+  state = {
+    authCheck: status.PENDING,
+  }
+
+  componentDidMount() {
+    Roles.check()
+      .then(() => this.setState({ authCheck: status.SUCCESS }))
+      .catch(() => this.setState({ authCheck: status.FAILURE }));
+  }
+
+  render() {
+    const { component: Component, ...rest } = this.props;
+
+    switch (this.state.authCheck) {
+      case status.PENDING:
+        return (
+          <BeatLoader
+            color={colors.highlight_primary}
+            sizeUnit={'px'}
+            size={15}
+          />
+        );
+
+      case status.SUCCESS:
+        return (
+          <Route {...rest} render={props => (<Component {...props} />)} />
+        );
+
+      case status.FAILURE:
+        return (
+          <Redirect to={{ pathname: '/login' }} />
+        );
+
+      default: // cos js be whack
+        return (
+          <Redirect to={{ pathname: '/login' }} />
+        );
+    }
+
+  }
+}
